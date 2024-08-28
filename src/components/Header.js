3dfd754c1486e5d62059a9823170134f18f539a8
Kaleb Sofer <kalebsofer@gmail.com>
2024-08-28 16:36:50 +0100

    updating header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,54 +45,56 @@ class Header extends Component {
         <div className="row aligner" style={{height: '100%'}}>
           <div className="col-md-12">
             <div>
-              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
-              <br/>
+              {/* <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span> */}
+              {/* <br/> */}
               <h1 className="mb-0">
                 <Typical steps={[name]} wrapper="p" />
               </h1>
               <div className="title-container">
                 <HeaderTitleTypeAnimation />
               </div>
-              <Switch
-                checked={this.state.checked}
-                onChange={this.onThemeSwitchChange}
-                offColor="#baaa80"
-                onColor="#353535"
-                className="react-switch mx-auto"
-                width={90}
-                height={40}
-                uncheckedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="twemoji:owl"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "20px",
-                      color: "#353239",
-                    }}
+              <div className="theme-switch-container">
+                <Switch
+                  checked={this.state.checked}
+                  onChange={this.onThemeSwitchChange}
+                  offColor="#baaa80"
+                  onColor="#353535"
+                  className="react-switch mx-auto"
+                  width={90}
+                  height={40}
+                  uncheckedIcon={
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:owl"
+                      data-inline="false"
+                      style={{
+                        display: "block",
+                        height: "100%",
+                        fontSize: 25,
+                        textAlign: "end",
+                        marginLeft: "20px",
+                        color: "#353239",
+                      }}
+                    ></span>
+                  }
+                  checkedIcon={
+                    <span
+                      className="iconify"
+                      data-icon="noto-v1:sun-with-face"
+                      data-inline="false"
+                      style={{
+                        display: "block",
+                        height: "100%",
+                        fontSize: 25,
+                        textAlign: "end",
+                        marginLeft: "10px",
+                        color: "#353239",
+                      }}
                   ></span>
-                }
-                checkedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="noto-v1:sun-with-face"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "10px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                id="icon-switch"
-              />
+                  }
+                  id="icon-switch"
+                />
+              </div>  
               <Menu
                 right
                 isOpen={this.state.menuOpen}
